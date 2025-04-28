1b7912174a9ed41b135df01dd2f50367e568b007
corey952 <w2013929@westminster.ac.uk>
2025-04-28 21:08:56 +0000

    Upload cleaned dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,14 @@ st.write(
     "Accidents"
 )
 
-df =  pd.read_csv("Traffic%20accidents_2019_Leeds.csv")
-st.write(df)
+data =  pd.read_csv("Traffic%20accidents_2019_Leeds.csv")
+#st.write(df)
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Accidents Throughout 2019")
+    st.line_chart(data.set_index('Accident Date')[''])
+
+with col2:
+    st.subheader("Total of Each Type Within 2019")
+    st.bar_chart(data.set_index('Accident Date')[''])
